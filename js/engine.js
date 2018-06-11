fb56ffc7931931f7d28ade356f881d730614396d
tUrlaj <39297165+tUrlaj@users.noreply.github.com>
2018-06-12 00:26:10 +0200

    6 Wersja gry!
    Poprawki.

--- js/engine.js
+++ js/engine.js
@@ -1,15 +1,15 @@
 /* Zmienne globalne */
-var myJumpGameSection;
-var myObstacles = [];
-var myHitSound;
-var myBackgroundMusic;
-var myScore;
-var life = 1000;
-var scoreMultiplerByHP = 1;
-var scoreMultiplerByMODS = 0;
-var DT = 0;
-var HR = 0;
-var HD = 0;
+let myJumpGameSection;
+let myObstacles = [];
+let myFailSound;
+let myBackgroundMusic;
+let myScore;
+let scoreMultiplerByHP = 1;
+let scoreMultiplerByMODS = 0;
+let life = 1000;
+let DT = 0;
+let HR = 0;
+let frequencyObstacles;
 
 /* Wczytywanie nowej gry po przegranej */
 function restartJumpGame() 
@@ -18,12 +18,14 @@ function restartJumpGame()
 	document.getElementById("scoreboard-win").style.display = "none";
 	myJumpGameArea.stop();
 	myWinnerAplauseSound.stop();
-	myBackgroundMusic.stop(); //tuuuuuuuuuuu
+	myBackgroundMusic.stop();
 	myJumpGameArea.clear();
 	myJumpGameArea = {};
 	myJumpGameSection = {};
 	myObstacles = [];
+	myFailSound = {};
 	myHitSound = {};
+	myJumpSound = {};
 	myBackgroundMusic = {};
 	myScore = {};
 	life = 1000;
@@ -31,6 +33,7 @@ function restartJumpGame()
 	scoreMultiplerByMODS = 0;
 	myFrames = {};
 	myLifes = {};
+	myMulti = {};
 	document.getElementById("canvasUnit").innerHTML = "";
 	startJumpGame()
 }
@@ -43,11 +46,14 @@ function startJumpGame()
 	document.getElementById("home-mods").style.display = "none";
 	myJumpGameArea = new gameArea();
 	myJumpGameSection = new component(64, 64, "img/char.gif", 50, 240, "image");
-	myScore = new component("20px", "Consolas", "white", 10, 50, "text");
+	myScore = new component("20px", "Consolas", "white", 10, 32, "text");
 	myFrames = new component("20px", "Consolas", "white", 185, 32, "text");
 	myLifes = new component("20px", "Consolas", "white", 410, 32, "text");
+	myMulti = new component("20px", "Consolas", "white", 600, 32, "text");
+	myFailSound = new sound("sounds/failSound.mp3");
 	myHitSound = new sound("sounds/hitObstacle.mp3");
 	myWinnerAplauseSound = new sound("sounds/winnerAplauseSound.mp3");
+	myJumpSound = new sound("sounds/jumpSound.mp3");
 	myBackgroundMusic = new sound("sounds/relaxingBackgroundMusic.mp3");
     myJumpGameArea.start();
 }
@@ -64,14 +70,16 @@ function gameArea()
     this.frameNumber = 0;
     this.start = function() 
 	{
-        this.interval = setInterval(updateGameArea, 5); //ilość klatek defautl: 20 ;; 38760 dla 5
+        this.interval = setInterval(updateGameArea, 5); //ilość klatek 5ms odswierzania dla 38760
 		window.addEventListener('keydown', function (e) //sterowanie za pomocą klawiatury
 		{
             myJumpGameArea.key = e.keyCode;
+			myJumpSound.play();
         })
 		window.addEventListener('keyup', function (e) 
 		{
             myJumpGameArea.key = false;
+			myJumpSound.stop();
         })
     },
     this.stop = function() 
@@ -89,12 +97,12 @@ function gameArea()
 function component(width, height, color, x, y, type) 
 {
     this.type = type;
-	if (this.type == "image") //mozliwość dodania skórek elementów
+	if ( this.type == "image" ) //mozliwość dodania skórek elementów
 	{
 		this.image = new Image();
 		this.image.src = color;
 	}
-	if (this.type == "text") 
+	if ( this.type == "text" ) 
 	{
 		this.text = new Text();
         this.text = color;
@@ -112,14 +120,14 @@ function component(width, height, color, x, y, type)
 	{
         ctx = myJumpGameArea.context;
 		/* Wyświetlanie score */
-		if (this.type == "text") 
+		if ( this.type == "text" ) 
 		{
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
         } 
 		/* Wyświetlanie elementów jako grafika */
-        if (this.type == "image") 
+        if ( this.type == "image" ) 
 		{
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         } 
@@ -138,7 +146,7 @@ function component(width, height, color, x, y, type)
     }
     this.hitBottom = function() 
 	{
-        var dno = myJumpGameArea.canvas.height - this.height;
+        let dno = myJumpGameArea.canvas.height - this.height;
         if (this.y > dno) 
 		{
             this.y = dno;
@@ -147,15 +155,15 @@ function component(width, height, color, x, y, type)
     }
 	this.crashWith = function(otherobj) 
 	{
-        var myleft = this.x;
-        var myright = this.x + (this.width);
-        var mytop = this.y;
-        var mybottom = this.y + (this.height);
-        var otherleft = otherobj.x;
-        var otherright = otherobj.x + (otherobj.width);
-        var othertop = otherobj.y;
-        var otherbottom = otherobj.y + (otherobj.height);
-        var crash = true;
+        let myleft = this.x;
+        let myright = this.x + (this.width);
+        let mytop = this.y;
+        let mybottom = this.y + (this.height);
+        let otherleft = otherobj.x;
+        let otherright = otherobj.x + (otherobj.width);
+        let othertop = otherobj.y;
+        let otherbottom = otherobj.y + (otherobj.height);
+        let crash = true;
         if ((mybottom < othertop) ||
                (mytop > otherbottom) ||
                (myright < otherleft) ||
@@ -167,7 +175,7 @@ function component(width, height, color, x, y, type)
     }
 }
 
-/* Obsługa plików muzycznych - background/hitsound */
+/* Obsługa plików muzycznych */
 function sound(src) 
 {
     this.sound = document.createElement("audio");
@@ -191,16 +199,17 @@ odtwarzanie / stopowanie muzyki po uderzeniu w pszeszkodę, wyświetlanie scoreb
 obsługę sterowania za pomocą spacji, zliczanie klatek oraz postępu gry */
 function everyInterval(n) 
 {
-    if ((myJumpGameArea.frameNumber / n) % 1 == 0)
-		{
+    if ( (myJumpGameArea.frameNumber / n) % 1 == 0 )
+	{
 		return true;
-		}
+	}
     return false;
 }
 
+/* Sprawdzanie włączonych modyfikacji */
 function checkbox()
 {
-	if(document.forms.form1.dt.checked==true)
+	if( document.forms.form1.dt.checked == true )
 	{
 		DT = 1;
 	}
@@ -208,7 +217,7 @@ function checkbox()
 	{
 		DT = 0;
 	}
-	if(document.forms.form1.hr.checked==true)
+	if( document.forms.form1.hr.checked == true)
 	{
 		HR = 1;
 	}
@@ -216,49 +225,42 @@ function checkbox()
 	{
 		HR = 0;
 	}
-	if(document.forms.form1.hd.checked==true)
-	{
-		HD = 1;
-	}
-	else
-	{
-		HD = 0;
-	}
 }
 
 function updateGameArea() 
 {
-    var x, y;
-    for (i = 0; i < myObstacles.length; i += 1) 
+    let x, y;
+    for ( i = 0; i < myObstacles.length; i += 1 ) 
 	{
 		if (myJumpGameSection.crashWith(myObstacles[i])) //po uderzeniu w przeszkode odejmij x hp ponizej 0 zakoncz gre
 		{
-			if(life > 0)
+			if( life > 0 ) // Jeżeli uderzymy w przeszkodę
 			{
-				life -= 2; // dodanie losowości 
+				myHitSound.play();
+				life -= Math.floor((Math.random() * 5) + 1); // dodanie losowości od 1-5 
 			}
 			else
 			{
 				/* Jeśli zycie spadnie do 0 pkt wyswietlany informacje o przegranej*/
-				myHitSound.play();
 				myBackgroundMusic.stop();
+				myFailSound.play();
 				myJumpGameArea.stop();
 				document.getElementById("scoreboard").style.display = "flex";
 				document.getElementById("status").innerHTML = "PORAŻKA!";
-				document.getElementById("score").innerHTML = "Twój całkowity wynik to: " + myScore.score * (scoreMultiplerByHP + scoreMultiplerByMODS);
+				document.getElementById("score").innerHTML = "Twój całkowity wynik to: " + (Math.round(myScore.score * (scoreMultiplerByHP + scoreMultiplerByMODS)*100)/100);
 				return;
 			}
         } 
 
 		/* Jeżeli wynik osiagnie 38400 punktów wyswietlamy informacje o wygranej */
-		if ( myScore.score == 38400) 
+		if ( myScore.score == 38400 ) 
 		{
 			myBackgroundMusic.stop();
 			myWinnerAplauseSound.play();
 			myJumpGameArea.stop();
 			document.getElementById("scoreboard-win").style.display = "flex";
 			document.getElementById("status-win").innerHTML = "WYGRANA!";
-			document.getElementById("score-win").innerHTML = "Twój całkowity wynik to: " + myScore.score * (scoreMultiplerByHP + scoreMultiplerByMODS);
+			document.getElementById("score-win").innerHTML = "Twój całkowity wynik to: " + (Math.round(myScore.score * (scoreMultiplerByHP + scoreMultiplerByMODS)*100)/100);
 			return;
 		} 
     }
@@ -266,48 +268,57 @@ function updateGameArea()
     myJumpGameArea.clear();
     myJumpGameArea.frameNumber += 1;
 	myScore.score +=1;
-    if (myJumpGameArea.frameNumber == 1 || everyInterval(150)) //co jaki czas przeszkoda // modyfikacja do HardRock'a
+	if ( DT == 1  ){frequencyObstacles = 100;}else{frequencyObstacles = 150;} //
+    if ( myJumpGameArea.frameNumber == 1 || everyInterval(frequencyObstacles) ) //co jaki czas przeszkoda // modyfikacja do HardRock'a
 	{
-        x = myJumpGameArea.canvas.width;
-        y = myJumpGameArea.canvas.height - 70; // modyfikacja do HardRock'a default 70 // do sprawdzenia warunku wygranej (-1)
-        myObstacles.push(new component(45, 83, "img/spikes.png", x, y, "image"));
+		if (HR == 1)
+		{
+			x = myJumpGameArea.canvas.width;
+			minHeight = 10;
+			maxHeight = 11;
+			height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
+			minGap = 80;
+			maxGap = 100;
+			gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
+			y = myJumpGameArea.canvas.height;
+			myObstacles.push(new component(45, x + height - gap, "img/spikes.png", x, y - height - gap, "image"));
+		}
+		else
+		{
+			x = myJumpGameArea.canvas.width;
+			y = myJumpGameArea.canvas.height - 70; // modyfikacja do HardRock'a default 70 // do sprawdzenia warunku wygranej (-1)
+			myObstacles.push(new component(45, 83, "img/spikes.png", x, y, "image"));
+		}
     }
-    for (i = 0; i < myObstacles.length; i += 1) 
+    for ( i = 0; i < myObstacles.length; i += 1 ) 
 	{
-		//if(myScore.score < 19850){ // myJumpGameArea.frameNumber < 1000 // znikanie przeszkód
-        myObstacles[i].x += -3; //predkość mapy // modyfikacja do DoubleTime'a
+        myObstacles[i].x += -3;
         myObstacles[i].update();
-		//}
-		//myObstacles[i].update();
     }
-	
 	
 	/* Mnoznik pkt dla punktów życia */
-	if(life == 1000){scoreMultiplerByHP = 1.6}
-	if(life <= 950){scoreMultiplerByHP = 1.5}
-	if(life <= 900){scoreMultiplerByHP = 1.4}
-	if(life <= 600){scoreMultiplerByHP = 1.3}
-	if(life <= 400){scoreMultiplerByHP = 1.2}
-	if(life <= 200){scoreMultiplerByHP = 1.1}
-	if(life == 0){scoreMultiplerByHP = 1}
+	if( life == 1000 ){scoreMultiplerByHP = 1.6}
+	if( life <= 950 ){scoreMultiplerByHP = 1.5}
+	if( life <= 900 ){scoreMultiplerByHP = 1.4}
+	if( life <= 600 ){scoreMultiplerByHP = 1.3}
+	if( life <= 400 ){scoreMultiplerByHP = 1.2}
+	if( life <= 200 ){scoreMultiplerByHP = 1.1}
+	if( life <= 0 ){scoreMultiplerByHP = 1}
 	/* Mnoznik pkt dla pojedyńczych modyfikacji */
-	if(DT == 1){scoreMultiplerByMODS = 0.10}
-	if(HR == 1){scoreMultiplerByMODS = 0.15}
-	if(HD == 1){scoreMultiplerByMODS = 0.25}
+	if( DT == 1 ){scoreMultiplerByMODS = 0.15}
+	if( HR == 1 ){scoreMultiplerByMODS = 0.20}
 	/* Mnoznik pkt dla wielu modyfikacji */
-	if(DT == 1 && HR == 1){scoreMultiplerByMODS = 0.25}
-	if(DT == 1 && HD == 1){scoreMultiplerByMODS = 0.35}
-	if(HD == 1 && HR == 1){scoreMultiplerByMODS = 0.40}
-	if(DT == 1 && HD == 1 && HR == 1){scoreMultiplerByMODS = 0.50}
-	
+	if( DT == 1 && HR == 1 ){scoreMultiplerByMODS = 0.35}
 	
 	/* HUB: */
 	myFrames.text="PROGRESS: " + (Math.round(myScore.score / 384 * 100) / 100) + "%";
 	myFrames.update();
-	myScore.text="SCORE: " + myScore.score + "*" + scoreMultiplerByHP + "*" + scoreMultiplerByMODS;        
+	myScore.text="SCORE: " + myScore.score;        
     myScore.update();
 	myLifes.text="LIFE: " + life + "/1000";        
     myLifes.update();
+	myMulti.text="SxHP: " + scoreMultiplerByHP + " + SxMODS: " + scoreMultiplerByMODS;        
+    myMulti.update();
 	
 	/* Aktualizacja paska postepu */
 	document.getElementById("myBar").style.width = myScore.score/384 + '%';
@@ -317,7 +328,7 @@ function updateGameArea()
     myJumpGameSection.speedY = 0; 
 	
 	/* sterowanie za pomocą spacji kod 32 */
-    if (myJumpGameArea.key && myJumpGameArea.key == 32) 
+    if ( myJumpGameArea.key && myJumpGameArea.key == 32 ) 
 	{
 		myJumpGameSection.speedY = -6; //predkość wznoszenia sie
 	} 
